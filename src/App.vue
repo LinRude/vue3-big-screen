4624167c26e748e2f986ce22bbb1ba8036843fc6
linrude <1320002856@qq.com>
2023-08-01 13:55:31 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+html {
+  height: 100%;
+  background-image: -webkit-radial-gradient(ellipse farthest-corner at center center, #1b44e4 0%, #020f3a 100%);
+  background-image: radial-gradient(ellipse farthest-corner at center center, #1b44e4 0%, #020f3a 100%);
+  cursor: move;
+}
+
+body {
+  width: 100%;
+  margin: 0;
+  overflow: hidden;
+}
+</style>
